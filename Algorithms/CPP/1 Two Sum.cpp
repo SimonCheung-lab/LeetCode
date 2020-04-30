89bff5e10b4b502c90c3c45365511d106ad8bd98
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-04-30 17:56:27 +0800

    Update 1 Two Sum.cpp

--- Algorithms/CPP/1 Two Sum.cpp
+++ Algorithms/CPP/1 Two Sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-	    unordered_map<int, int> map;
-	    vector<int> result;
-	    for (int i = 0; i < nums.size(); ++i) {
-		    if (map.find(target - nums[i]) != map.end()) {
-			    result.push_back(i);
-			    result.push_back(map[target - nums[i]]);
-          # assume only one solution
-          break;
-		    }
-		    else {
-			    map[nums[i]] = i;
-		    }
+        unordered_map<int, int> map;
+	vector<int> result;
+	for (int i = 0; i < nums.size(); ++i) {
+	    if (map.find(target - nums[i]) != map.end()) {
+	        result.push_back(i);
+		result.push_back(map[target - nums[i]]);
+                # assume only one solution
+                break;
 	    }
+            else {
+	        map[nums[i]] = i;
+	    }
+	}
 
-	    return result;
+	return result;
     }
 };
