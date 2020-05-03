62d0ba8155e6c0c3e37895aab55651f575855846
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-03 22:19:33 +0800

    Update 437 Path Sum III.py

--- Algorithms/Python/437 Path Sum III.py
+++ Algorithms/Python/437 Path Sum III.py
@@ -5,7 +5,7 @@
 #         self.left = None
 #         self.right = None
 
-# Double recursion
+# Solution 1: Double recursion
 class Solution:
     result = 0
     def pathSum(self, root: TreeNode, sum: int) -> int:
@@ -24,3 +24,28 @@ class Solution:
             self.result += 1
         self.dfs(node.left, sum - node.val)
         self.dfs(node.right, sum - node.val)
+        
+        
+# Solution 2: prefix sum + deep first search(stack)       
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        self.result = 0
+        # statistic of prefix sums 
+        cache = {0: 1}
+
+        def dfs(node, prefix_sum):
+            if node is None:
+                return 
+        
+            prefix_sum += node.val
+            self.result += cache.get(prefix_sum - sum, 0)
+            cache[prefix_sum] = cache.get(prefix_sum, 0) + 1
+
+            dfs(node.left, prefix_sum)
+            dfs(node.right, prefix_sum)
+            # before out the stack, remove the prefix sum of current node
+            cache[prefix_sum] -= 1
+
+        dfs(root, 0)
+
+        return self.result
