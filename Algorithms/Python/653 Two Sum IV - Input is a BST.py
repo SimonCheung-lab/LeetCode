657686e42cadfb862405aa17b8a2a880a992722d
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-05 22:15:41 +0800

    Create 653 Two Sum IV - Input is a BST.py

---
+++ Algorithms/Python/653 Two Sum IV - Input is a BST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        # in order traverse
+        def in_order_traverse(node):
+            if node is None:
+                return
+            
+            in_order_traverse(node.left)
+            missing = object()
+            if self.cache.get(k - node.val, missing) != missing:
+                self.result = True
+                return
+            else:
+                self.cache[node.val] = self.i
+                self.i += 1
+            in_order_traverse(node.right)
+
+        self.i = 0
+        self.cache = {}
+        self.result = False
+        in_order_traverse(root)
+        return self.result
