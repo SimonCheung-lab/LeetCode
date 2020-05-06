f317568dfbb871c42b42549051c870b9ff270268
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-06 16:33:51 +0800

    Update 15 3Sum.py

--- Algorithms/Python/15 3Sum.py
+++ Algorithms/Python/15 3Sum.py
@@ -20,3 +20,30 @@ class Solution:
         self.three = [0] * 3
         find_three(0, 0)
         return self.result
+
+    
+# solution 2
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        n = len(nums)
+        for i in range(n):
+            if i == 0 or nums[i] > nums[i - 1]:
+                left, right = i + 1, n - 1
+                while left < right:
+                    s = nums[i] + nums[left] + nums[right]
+                    if s == 0:
+                        result.append([nums[i], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif s > 0:
+                        right -= 1
+                    else:
+                        left += 1
+        return result 
+    
