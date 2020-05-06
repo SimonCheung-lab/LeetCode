28fe781e3932d924ad85627903dd8cf1d100856a
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-06 18:47:07 +0800

    Create 454 4Sum II.py

---
+++ Algorithms/Python/454 4Sum II.py
@@ -0,0 +1,41 @@
+# out of time
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        A.sort()
+        B.sort()
+        C.sort()
+        D.sort()
+        n = len(A)
+        result = 0
+        i = 0
+        while i < n:
+            j = 0
+            if A[i] + B[0] + C[0] + D[0] <= 0:
+                while j < n:
+                    k, l = 0, n - 1
+                    if A[i] + B[j] + C[0] + D[0] <= 0:
+                        while k < n and l >= 0:
+                            s = A[i] + B[j] + C[k] + D[l]
+                            if s == 0:
+                                # result += 1
+                                k += 1
+                                c1 = 1
+                                while k < n and C[k] == C[k - 1]:
+                                    k += 1
+                                    c1 += 1
+                                l -= 1
+                                c2 = 1
+                                while l >= 0 and D[l] == D[l + 1]:
+                                    l -= 1
+                                    c2 += 1
+                                # print(c1, c2)
+                                result += c1 * c2
+                            elif s < 0:
+                                k += 1
+                            else:
+                                l -= 1
+                    # update j
+                    j += 1
+            # update i
+            i += 1
+        return result
