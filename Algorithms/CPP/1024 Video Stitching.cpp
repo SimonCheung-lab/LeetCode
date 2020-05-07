3a56a96dc6dab0d1db711803644a4044151f1dd8
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-07 17:22:37 +0800

    Create 1024 Video Stitching.cpp

---
+++ Algorithms/CPP/1024 Video Stitching.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int videoStitching(vector<vector<int>>& clips, int T) {
+        // import
+        sort(clips.begin(), clips.end(), [](vector<int>& c1, vector<int>& c2){
+            return c1[0] < c2[0] || (c1[0] == c2[0] && c1[1] < c2[1]);
+        });
+
+        // dp[i] is the minimal clips to cover [0, i]
+        vector<int> dp(T + 1, -1);
+        for (auto c : clips){
+            if (c[0] > T){
+                continue;
+            }
+
+            if (c[0] == 0){
+                for (int i = 0; i <= min(c[1], T); ++i){
+                    dp[i] = 1;
+                }
+                continue;
+            }
+
+            if (dp[c[0]] == -1){
+                // just break
+                break;
+            }
+
+            int m = dp[c[0]];
+            for (int i = c[0] + 1; i <= min(c[1], T); ++i){
+                if (dp[i] > 0){
+                    m = min(m, dp[i]);
+                }
+                else{
+                    dp[i] = m + 1;
+                }
+            }
+        }
+        return dp[T];
+    }
+};
