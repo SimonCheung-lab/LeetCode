e64580ae748f55ff69e75841dd1df38bd3e19387
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-09 11:58:17 +0800

    Update 977 Squares of a Sorted Array.py

--- Algorithms/Python/977 Squares of a Sorted Array.py
+++ Algorithms/Python/977 Squares of a Sorted Array.py
@@ -1,4 +1,21 @@
+# solution 1
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
-        A.sort(key=lambda x : abs(x))
-        return list(map(lambda x: x**2, A))
+        A.sort(key=lambda x: abs(x))
+        return list(map(lambda x: x ** 2, A))
+
+# solution 2    
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        n = len(A)
+        result = [0] * n
+        i, j, k = 0, n - 1, n - 1
+        while i <= j:
+            if abs(A[i]) >= abs(A[j]):
+                result[k] = A[i] ** 2
+                i += 1
+            else:
+                result[k] = A[j] ** 2
+                j -= 1
+            k -= 1
+        return result
