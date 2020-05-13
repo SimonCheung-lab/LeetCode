9a1044fd74d32ce6a18e44478cb63a7c8e4c92fa
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-13 12:26:05 +0800

    Update 315 Count of Smaller Numbers After Self.py

--- Algorithms/Python/315 Count of Smaller Numbers After Self.py
+++ Algorithms/Python/315 Count of Smaller Numbers After Self.py
@@ -1,5 +1,6 @@
 import bisect
 
+# solution 1: bisect
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         ans = []
@@ -9,3 +10,34 @@ class Solution:
             sorted_list.insert(i, n)
             ans.append(i)
         return ans[::-1]
+
+# solution 2: merge sort    
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
+
+            mid = len(arr) // 2
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+            return merge(left, right)
+
+        def merge(left, right):
+            i, j = 0, 0
+            tmp = []
+            while i < len(left) or j < len(right):
+                if j == len(right) or i < len(left) and left[i][1] <= right[j][1]:
+                    ans[left[i][0]] += j
+                    tmp.append(left[i])
+                    i += 1
+                else:
+                    tmp.append(right[j])
+                    j += 1
+            
+            return tmp
+
+        ans = [0] * len(nums)
+        arr = [(i, n) for i, n in enumerate(nums)]
+        merge_sort(arr)
+        return ans    
