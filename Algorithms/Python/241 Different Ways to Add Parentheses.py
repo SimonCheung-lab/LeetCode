25a6c6782c6984daa820f64ede2459a2779ced9f
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-16 17:41:22 +0800

    Create 241 Different Ways to Add Parentheses.py

---
+++ Algorithms/Python/241 Different Ways to Add Parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def diffWaysToCompute(self, input: str) -> List[int]:
+        if input.isdigit():
+            return [int(input)]
+        
+        ans = []
+        for i, s in enumerate(input):
+            if s in ['+', '-', '*']:
+                left = self.diffWaysToCompute(input[:i])
+                right = self.diffWaysToCompute(input[i + 1:])
+                for l in left:
+                    for r in right:
+                        # ans.append(eval(str(l) + s + str(r)))
+                        if s == '+':
+                            ans.append(l + r)
+                        elif s == '-':
+                            ans.append(l - r)
+                        else:
+                            ans.append(l * r)
+        
+        return ans
