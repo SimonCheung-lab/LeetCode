32a98760c64d664c5344914c880920352dbaac70
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-22 00:01:29 +0800

    Update 41 First Missing Positive.py

--- Algorithms/Python/41 First Missing Positive.py
+++ Algorithms/Python/41 First Missing Positive.py
@@ -1,12 +1,9 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        if len(nums) == 0:
+        max_num = len(nums)
+        if max_num == 0:
             return 1
-
-        max_num = max(nums)
-        if max_num <= 0:
-            return 1
-        for i in range(1, max_num):
+        for i in range(1, max_num + 1):
             if i not in nums:
                 return i
         return max_num + 1
