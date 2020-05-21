8a189127dc40b3f264c35e8a1fc90a8611fd5b52
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-22 00:02:56 +0800

    Update 41 First Missing Positive.cpp

--- Algorithms/CPP/41 First Missing Positive.cpp
+++ Algorithms/CPP/41 First Missing Positive.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        if (nums.size() == 0){
-            return 1;
-        }
-        
-        int max_num = *std::max_element(nums.begin(), nums.end());
-        if (max_num <= 0){
+        int max_num = nums.size();
+        if (max_num == 0){
             return 1;
         }
 
-        for (int i = 1; i < max_num; ++i){
+        for (int i = 1; i <= max_num; ++i){
             if (std::find(nums.begin(), nums.end(), i) == nums.end()){
                 return i;
             }
