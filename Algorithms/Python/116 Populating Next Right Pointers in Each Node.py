87de8f86cba104931f896e3588fe3bfc36d9e2c6
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-28 10:24:37 +0800

    Create 116 Populating Next Right Pointers in Each Node.py

---
+++ Algorithms/Python/116 Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        def connect_level(nodes):
+            if len(nodes) == 0:
+                return
+            
+            sublevel = []
+            for i in range(len(nodes) - 1):
+                nodes[i].next = nodes[i + 1]
+                if nodes[i].left:
+                    sublevel.append(nodes[i].left)
+                if nodes[i].right:
+                    sublevel.append(nodes[i].right)
+            
+            nodes[-1].next = None
+            if nodes[-1].left:
+                sublevel.append(nodes[-1].left)
+            if nodes[-1].right:
+                sublevel.append(nodes[-1].right)
+            connect_level(sublevel)
+        
+        if root is None:
+            return None
+
+        nodes = []
+        nodes.append(root)
+        connect_level(nodes)
+        return root
