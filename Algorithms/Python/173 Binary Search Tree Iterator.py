309377f18de1dbc1ac129e025001a52aaf4e1335
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-29 16:38:47 +0800

    Update 173 Binary Search Tree Iterator.py

--- Algorithms/Python/173 Binary Search Tree Iterator.py
+++ Algorithms/Python/173 Binary Search Tree Iterator.py
@@ -38,3 +38,58 @@ class BSTIterator:
 # obj = BSTIterator(root)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()
+
+# solution 2, esentially same as solution 1
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.data = []
+        self.index = -1
+        self.inorder(root)
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        self.index += 1
+        return self.data[self.index]
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return self.index < len(self.data) - 1
+
+    def inorder(self, root):
+        if root is None:
+            return
+
+        self.inorder(root.left)
+        self.data.append(root.val)
+        self.inorder(root.right)
+
+# solution 3        
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        self.left_most(root)
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        node = self.stack.pop()
+        self.left_most(node.right)
+        return node.val
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.stack) > 0
+
+    def left_most(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left        
