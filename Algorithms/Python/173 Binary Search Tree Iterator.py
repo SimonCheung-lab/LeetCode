5c8d407e3f8c06da85bc4e9eaa3838c299b041d4
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-29 14:21:34 +0800

    Create 173 Binary Search Tree Iterator.py

---
+++ Algorithms/Python/173 Binary Search Tree Iterator.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+import queue
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.data = queue.Queue()
+        self.inorder(root)
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        return self.data.get()
+
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return not self.data.empty()
+
+    def inorder(self, root):
+        if root is None:
+            return
+
+        self.inorder(root.left)
+        self.data.put(root.val)
+        self.inorder(root.right)
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
