a392f44ae1b3ac33de6c692f20ef0aad237d6c53
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-30 17:10:46 +0800

    Create 951 Filp Equivalent Binary Trees.py

---
+++ Algorithms/Python/951 Filp Equivalent Binary Trees.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+        self.flip(root1)
+        self.flip(root2)
+
+        return self.equal(root1, root2)
+
+    def equal(self, root1, root2):
+        if root1 is None and root2 is None:
+            return True
+
+        if root1 and root2 and root1.val == root2.val:
+            return self.equal(root1.left, root2.left) and self.equal(root1.right, root2.right)
+
+        return False
+
+    def flip(self, root):
+        if root is None:
+            return
+
+        if root.right is None or (root.left and root.left.val > root.right.val):
+            # swap
+            node = root.left
+            root.left = root.right
+            root.right = node
+
+        self.flip(root.left)
+        self.flip(root.right)
