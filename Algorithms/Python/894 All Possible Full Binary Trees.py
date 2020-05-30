ac62f81fb0511cbc1a81d502605b1bea2ae877ab
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-30 23:00:16 +0800

    Create 894 All Possible Full Binary Trees.py

---
+++ Algorithms/Python/894 All Possible Full Binary Trees.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        def buildFBT(n):
+            if n == 1:
+                return [TreeNode(0)]
+
+            ans = []
+            for i in range(1, n, 2):
+                # left + right = n - 1
+                # leave one as root
+                left = buildFBT(i)
+                right = buildFBT(n - 1 - i)
+                for l in left:
+                    for r in right:
+                        root = TreeNode(0)
+                        root.left = l
+                        root.right = r
+                        ans.append(root)
+            return ans
+
+        if N % 2 == 0:
+            return []
+
+        return buildFBT(N)
