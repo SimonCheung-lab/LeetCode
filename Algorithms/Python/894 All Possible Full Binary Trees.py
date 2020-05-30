5dee612a7353c9101165546c5484dbce35669ad3
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-05-30 23:22:05 +0800

    Update 894 All Possible Full Binary Trees.py

--- Algorithms/Python/894 All Possible Full Binary Trees.py
+++ Algorithms/Python/894 All Possible Full Binary Trees.py
@@ -29,3 +29,28 @@ class Solution:
             return []
 
         return buildFBT(N)
+
+    
+# solution 2    
+class Solution:
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        def buildFBT(n):
+            if n not in cache:
+                ans = []
+                for i in range(1, n, 2):
+                    left = buildFBT(i)
+                    right = buildFBT(n - 1 - i)
+                    for l in left:
+                        for r in right:
+                            root = TreeNode(0)
+                            root.left = l
+                            root.right = r
+                            ans.append(root)
+                cache[n] = ans
+            return cache[n]
+
+        if N % 2 == 0:
+            return []
+
+        cache = {1:[TreeNode(0)]}
+        return buildFBT(N)    
