86e6040ec956f5dfb226be775a81c188de741a0d
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-06-01 13:20:25 +0800

    Create 377 Combination Sum IV.py

---
+++ Algorithms/Python/377 Combination Sum IV.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0 or target < 0:
+            return 0
+
+        dp = [0 for _ in range(target + 1)]
+        dp[0] = 1
+        for i in range(target + 1):
+            for n in nums:
+                if i - n >= 0:
+                    dp[i] += dp[i - n]
+        return dp[target]
