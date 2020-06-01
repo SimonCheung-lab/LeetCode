9dddd0a8a025328edb830e7e7e0a6e93bb88663d
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-06-01 17:28:46 +0800

    Update 32 Longest Valid Parentheses.py

--- Algorithms/Python/32 Longest Valid Parentheses.py
+++ Algorithms/Python/32 Longest Valid Parentheses.py
@@ -13,3 +13,21 @@ class Solution:
         for i in range(1, len(stack)):
             ans = max(ans, stack[i][0] - stack[i - 1][0] - 1)
         return ans
+
+
+# solution 2, more faster    
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        s = '#' + s + '#'
+        stack = []
+        for i in range(len(s)):
+            if s[i] == ')' and stack and s[stack[-1]] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+        
+        # find the max index difference
+        ans = 0
+        for i in range(1, len(stack)):
+            ans = max(ans, stack[i] - stack[i - 1] - 1)
+        return ans    
