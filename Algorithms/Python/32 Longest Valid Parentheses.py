b2f1803d1c04d5ce3f43105f789646563e735eb2
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-06-01 20:58:28 +0800

    Update 32 Longest Valid Parentheses.py

--- Algorithms/Python/32 Longest Valid Parentheses.py
+++ Algorithms/Python/32 Longest Valid Parentheses.py
@@ -31,3 +31,34 @@ class Solution:
         for i in range(1, len(stack)):
             ans = max(ans, stack[i] - stack[i - 1] - 1)
         return ans    
+
+    
+# solution 3, space cost O(1)    
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        left, right = 0, 0
+        ans = 0
+        # from left to right
+        for c in s:
+            if c == '(':
+                left += 1
+            elif c == ')':
+                right += 1
+                if right == left:
+                    ans = max([ans, left + right])
+                elif right > left:
+                    left = right = 0
+        
+        left = right = 0
+        # from right to left
+        for c in s[::-1]:
+            if c == ')':
+                right += 1
+            elif c == '(':
+                left += 1
+                if left == right:
+                    ans = max([ans, left + right])
+                elif left > right:
+                    left = right = 0
+
+        return ans    
