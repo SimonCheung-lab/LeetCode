b319948c5aa2459d8877083471827f571f28d099
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-06-06 13:25:35 +0800

    Update 300 Longest Increasing Subsequence.c

--- Algorithms/C/300 Longest Increasing Subsequence.c
+++ Algorithms/C/300 Longest Increasing Subsequence.c
@@ -21,3 +21,42 @@ int lengthOfLIS(int* nums, int numsSize){
     free(dp);
     return ans;
 }
+
+
+// O(nlogn)
+int lengthOfLIS(int* nums, int numsSize){
+    if (nums == NULL || numsSize <= 0) {
+        return 0;
+    }
+
+    int* dp = calloc(numsSize, sizeof(int));
+    dp[0] = nums[0];
+    int n = 1;
+    for (int i = 1; i < numsSize; ++i) {
+        if (nums[i] > dp[n - 1]) {
+            dp[n++] = nums[i];
+        }
+        else {
+            int left, right;
+            left = 0;
+            right = n - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (dp[mid] == nums[i]) {
+                    break;
+                }
+                else if (dp[mid] < nums[i]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid;
+                }
+            }
+            if (left == right) {
+                dp[left] = nums[i];
+            }
+        }
+    }
+    free(dp);
+    return n;
+}
