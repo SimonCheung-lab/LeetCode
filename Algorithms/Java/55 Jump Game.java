2a08c343a024cd14ef8baf622731ddd2e386f640
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-07-25 13:02:10 +0800

    Create 55 Jump Game.java

---
+++ Algorithms/Java/55 Jump Game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+            }
+        }
+
+        return rightmost >= n - 1;
+    }
+}
