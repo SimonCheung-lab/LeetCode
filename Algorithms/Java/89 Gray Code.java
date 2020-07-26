80c9522e2b25e3619b93d9f56f50be8ae2176ab1
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-07-26 17:38:11 +0800

    Create 89 Gray Code.java

---
+++ Algorithms/Java/89 Gray Code.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        int head = 1;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = list.size() - 1; j >=0; --j) {
+                list.add(head + list.get(j));
+            }
+
+            head <<= 1;
+        }
+
+        return list;
+    }
+}
