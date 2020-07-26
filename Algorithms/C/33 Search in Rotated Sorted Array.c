6a1db5f56e68ac729dc901b7138e1b124570f6c9
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-07-26 11:52:32 +0800

    Create 33 Search in Rotated Sorted Array.c

---
+++ Algorithms/C/33 Search in Rotated Sorted Array.c
@@ -0,0 +1,31 @@
+int search(int* nums, int numsSize, int target){
+    int left, right, mid;
+    left = 0;
+    right = numsSize - 1;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
