99f9c6057eb485efdda691e75dde7e5d6ef2dc82
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-07-26 13:11:41 +0800

    Create 81 Search in Rotated Sorted Array II.java

---
+++ Algorithms/Java/81 Search in Rotated Sorted Array II.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int left, right, mid;
+        left = 0;
+        right = n - 1;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[left] == nums[mid]) {
+                left++;
+                continue;
+            }
+
+            if (nums[right] == nums[mid]) {
+                right--;
+                continue;
+            }
+
+            if (nums[left] < nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
