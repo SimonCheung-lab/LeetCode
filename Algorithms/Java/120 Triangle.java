416e4bd5ef57bac562e29294d801fadc95fdc092
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2020-08-01 17:43:02 +0800

    Create 120 Triangle.java

---
+++ Algorithms/Java/120 Triangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() == 0) {
+            return 0;
+        }
+
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle.get(i).size(); ++j) {
+                triangle.get(i).set(j, Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)) + triangle.get(i).get(j));
+            }
+        }
+
+        return triangle.get(0).get(0);
+    }
+}
