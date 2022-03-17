84f07b5654b073cc8a528da3d89579edc9855450
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2022-03-17 18:17:55 +0800

    Create 191. Number of 1 Bits.c

---
+++ Algorithms/C/191. Number of 1 Bits.c
@@ -0,0 +1,14 @@
+int hammingWeight(uint32_t n) {
+        /* 
+        / 0000 0001 0010 0011 0100 0101 0110 0111 
+        / 1000 1001 1010 1011 1100 1101 1110 1111
+        */
+        char table[] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4}; 
+        int count = 0;
+        while (n)
+        {
+            count += table[n & 0xf];
+            n = n >> 4;
+        }
+        return count;
+}
