9a1b3e5be89ce4148bb918d7ee902b68a2c28d14
SimonCheung-lab <59137455+SimonCheung-lab@users.noreply.github.com>
2022-03-17 21:08:42 +0800

    Update 50 Pow.c

--- Algorithms/C/50 Pow.c
+++ Algorithms/C/50 Pow.c
@@ -18,3 +18,28 @@ double myPow(double x, int n){
 
     return x * myPow(x, n - 1);
 }
+
+// method 2
+// 一、注意边界的处理；二、使用分治算法，减少乘法次数，提升效率；
+double myPow(double x, int n){
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    if (n > 0)
+    {
+        if (n % 2 == 0)
+        {
+            return myPow(x * x, n / 2);
+        }
+        return x * myPow(x, n - 1);
+    }
+
+    if (n == INT_MIN)
+    {
+        return 1.0 / (x * myPow(x, INT_MAX));
+    }
+
+    return 1.0 / myPow(x, -n);
+}
